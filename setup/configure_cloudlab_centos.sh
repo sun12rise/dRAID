f1f72c23068b993f18ac9397d8e7423eb8437171
sun12rise <1600996310@qq.com>
2024-10-21 15:39:04 +0800

    test0

--- setup/configure_cloudlab_centos.sh
+++ setup/configure_cloudlab_centos.sh
@@ -13,6 +13,22 @@ sudo sysctl -w vm.nr_hugepages=32768
 git clone https://github.com/kyleshu/draid-spdk.git
 cd draid-spdk
 git submodule update --init
+sudo apt-get update
+ruby -v
+# 安装 rbenv（如果尚未安装）
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-installer | bash
+
+# 添加 rbenv 到 PATH
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# 安装 Ruby 3.0.0
+rbenv install 3.0.0
+rbenv global 3.0.0
+#Ruby 版本升级完成后，安装 Bundler：
+gem install bundler
+
 sudo scripts/pkgdep.sh --all
 cd ..
 git clone https://github.com/axboe/fio
@@ -22,6 +38,32 @@ sudo make install
 sudo ldconfig
 cd ..
 cd draid-spdk
+
+sudo apt update
+sudo apt install nasm
+
+sudo apt remove nasm
+y
+
+#下载 NASM 新版本源代码：
+cd /tmp
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.gz
+tar -xzf nasm-2.15.05.tar.gz
+cd nasm-2.15.05
+./autogen.sh
+./configure --prefix=/usr/local
+make
+sudo make install
+nasm -v
+which nasm
+echo $PATH
+hash -r
+nasm -v
+
+#输出 NASM version 2.15.05 compiled on Oct 17 2024
+#返回spdk
+
+cd  ~/draid-spdk
 ./configure --with-rdma --with-fio=$HOME/fio --with-isal --with-raid5 --disable-unit-tests
 make
 cd ..
@@ -36,3 +78,14 @@ cd ..
 
 # clone customized version of rocksdb for testing
 git clone https://github.com/kyleshu/rocksdb.git
+
+
+# 要添加的内容
+# 添加内容到 ~/.bashrc
+echo "export PKG_CONFIG_PATH=~/draid-spdk/build/lib/pkgconfig:\$PKG_CONFIG_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=~/draid-spdk/build/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
+source ~/.bashrc
+
+
+
+
--- setup/parse_manifest.py
+++ setup/parse_manifest.py
@@ -9,20 +9,21 @@ if __name__ == '__main__':
     ip_addrs = open(sys.argv[3], "w")
     ip_addrs2 = open(sys.argv[4], "w")
     rspec = tree.getroot()
+    
     for node in rspec:
         if node.tag == '{http://www.geni.net/resources/rspec/3}node':
-            if node[0][0].attrib['name'] == "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD":
-                ubuntu = node[7][0].attrib['hostname']
-                continue
-            ip_addrs.write(node[3][0].attrib['address'] + '\n')
-            ip_addrs2.write(node[2][0].attrib['address'] + '\n')
-            for entry in node[7]:
+            ubuntu = node[3][0].attrib['hostname']
+            ip_addrs.write(node[2][0].attrib['address'] + '\n')
+            
+            for entry in node[3]:
                 try:
                     hosts.write(entry.attrib['hostname'] + '\n')
                     break
                 except KeyError:
                     continue
+    
     print(ubuntu)
     hosts.close()
     ip_addrs.close()
     ip_addrs2.close()
+
