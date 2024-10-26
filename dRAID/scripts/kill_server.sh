00447fad99e0f73ddc9bb66f1fbac0f9dae6ec3b
sun12rise <1600996310@qq.com>
2024-10-26 15:25:48 +0800

    Try dRAID-AE

--- dRAID/scripts/kill_server.sh
+++ dRAID/scripts/kill_server.sh
@@ -2,7 +2,7 @@
 
 username=$1
 
-sudo -E kill -9 $(ps aux | grep '[r]aid' | awk '{print $2}')
-sudo -E kill -9 $(ps aux | grep '[n]vmf_tgt' | awk '{print $2}')
+sudo kill -9 $(ps aux | grep '[r]aid' | awk '{print $2}')
+sudo kill -9 $(ps aux | grep '[n]vmf_tgt' | awk '{print $2}')
 cd /users/$username/draid-spdk
-sudo -E ./scripts/setup.sh reset
+sudo ./scripts/setup.sh reset
--- setup/configure_cloudlab_centos.sh
+++ setup/configure_cloudlab_centos.sh
@@ -11,39 +11,9 @@ sudo sysctl -w vm.nr_hugepages=32768
 
 # clone customized version of SPDK and FIO for dRAID
 git clone https://github.com/kyleshu/draid-spdk.git
-cd draid-spdk
-git submodule update --init
-sudo apt-get update
-ruby -v
-# 安装 rbenv（如果尚未安装）
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-installer | bash
 
-# 添加 rbenv 到 PATH
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
 
-# 安装 Ruby 3.0.0
-rbenv install 3.0.0
-rbenv global 3.0.0
-#Ruby 版本升级完成后，安装 Bundler：
-gem install bundler
-
-sudo scripts/pkgdep.sh --all
-cd ..
-git clone https://github.com/axboe/fio
-cd fio
-make
-sudo make install
-sudo ldconfig
-cd ..
-cd draid-spdk
-
-sudo apt update
-sudo apt install nasm
-
-sudo apt remove nasm -y
-
+cd ~/draid-spdk
 #下载 NASM 新版本源代码：
 cd /tmp
 wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.gz
@@ -54,15 +24,27 @@ cd nasm-2.15.05
 make
 sudo make install
 nasm -v
-which nasm
-echo $PATH
-hash -r
-nasm -v
 
-#输出 NASM version 2.15.05 compiled on Oct 17 2024
-#返回spdk
 
-cd  ~/draid-spdk
+cd ~/draid-spdk
+#返回到 draid- spdk
+wget https://github.com/linux-rdma/rdma-core/releases/download/v53.1/rdma-core-53.1.tar.gz
+tar -xzvf rdma-core-53.1.tar.gz
+cd rdma-core-53.1
+
+
+
+cd ~/draid-spdk
+git submodule update --init
+sudo scripts/pkgdep.sh --all
+cd ..
+git clone https://github.com/axboe/fio
+cd fio
+make
+sudo make install
+sudo ldconfig
+cd ..
+cd draid-spdk
 ./configure --with-rdma --with-fio=$HOME/fio --with-isal --with-raid5 --disable-unit-tests
 make
 cd ..
@@ -77,14 +59,3 @@ cd ..
 
 # clone customized version of rocksdb for testing
 git clone https://github.com/kyleshu/rocksdb.git
-
-
-# 要添加的内容
-# 添加内容到 ~/.bashrc
-echo "export PKG_CONFIG_PATH=~/draid-spdk/build/lib/pkgconfig:\$PKG_CONFIG_PATH" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=~/draid-spdk/build/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
-source ~/.bashrc
-
-
-
-
